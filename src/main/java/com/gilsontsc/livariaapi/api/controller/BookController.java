b8c89ec6cdd39962aed906e4aee8bb2587cc8820
gilsontsc <gilson.costa@dcx.ufpb.br>
2020-11-28 12:01:40 -0300

    Inserindo a dependencia do ModelMapper

--- src/main/java/com/gilsontsc/livariaapi/api/controller/BookController.java
+++ src/main/java/com/gilsontsc/livariaapi/api/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.gilsontsc.livariaapi.api.controller;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,22 +20,16 @@ public class BookController {
 	@Autowired
 	private BookService service;
 	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public BookDTO create(@RequestBody BookDTO dto) {
-		Book entity = Book.builder()
-						  .author(dto.getAuthor())
-						  .title(dto.getTitle())
-						  .isbn(dto.getIsbn())
-						  .build();
+		Book entity = modelMapper.map(dto, Book.class);
 		
 		entity = service.save(entity);
 		
-		return BookDTO.builder()
-					  .id(entity.getId())
-					  .author(entity.getAuthor())
-					  .title(entity.getTitle())
-					  .isbn(entity.getIsbn())
-					  .build();
+		return modelMapper.map(entity, BookDTO.class);
 	}
 }
