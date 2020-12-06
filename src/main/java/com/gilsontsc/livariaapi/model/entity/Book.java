079a93d09f895356c92b5f92faeb98e5bf808b7f
gilsontsc <gilson.costa@dcx.ufpb.br>
2020-12-06 11:23:27 -0300

    Verificando se existe livro na base de dados com isbn no repositorio

--- src/main/java/com/gilsontsc/livariaapi/model/entity/Book.java
+++ src/main/java/com/gilsontsc/livariaapi/model/entity/Book.java
@@ -1,19 +1,32 @@
 package com.gilsontsc.livariaapi.model.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Book {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String title;
+	
 	private String author;
+	
 	private String isbn;
 	
 }
---
+++ src/test/java/com/gilsontsc/livariaapi/model/repository/BookRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.gilsontsc.livariaapi.model.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.gilsontsc.livariaapi.model.entity.Book;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DataJpaTest
+public class BookRepositoryTest {
+
+	@Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    BookRepository repository;
+
+    @Test
+    @DisplayName("Deve retornar verdadeiro quando existir um livro na base com o isbn informado")
+    public void returnTrueWhenIsbnExists(){
+        //cenario
+        String isbn = "123";
+        Book book = createNewBook(isbn);
+        entityManager.persist(book);
+
+        //execucao
+        boolean exists = repository.existsByIsbn(isbn);
+
+        //verificacao
+        assertThat(exists).isTrue();
+    }
+    
+    @Test
+    @DisplayName("Deve retornar false quando não existir um livro na base com o isbn informado")
+    public void returnFalseWhenIsbnDoesntExist(){
+        //cenario
+        String isbn = "123";
+        
+        //execucao
+        boolean exists = repository.existsByIsbn(isbn);
+
+        //verificacao
+        assertThat(exists).isFalse();
+    }
+    
+    public static Book createNewBook(String isbn) {
+        return Book.builder().title("Aventuras").author("Fulano").isbn(isbn).build();
+    }
+    
+}
