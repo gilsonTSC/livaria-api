47ec36147ecc534a9d514be10f74308bafe980d3
gilsontsc <gilson.costa@dcx.ufpb.br>
2020-12-09 18:30:42 -0300

    Implementado o teste de obter livro por id no service

--- src/main/java/com/gilsontsc/livariaapi/service/impl/BookServiceImpl.java
+++ src/main/java/com/gilsontsc/livariaapi/service/impl/BookServiceImpl.java
@@ -28,7 +28,7 @@ public class BookServiceImpl implements BookService {
 
 	@Override
 	public Optional<Book> getById(Long id) {
-		return null;
+		return this.repository.findById(id);
 	}
 
 	@Override
--- src/test/java/com/gilsontsc/livariaapi/service/BookServiceTest.java
+++ src/test/java/com/gilsontsc/livariaapi/service/BookServiceTest.java
@@ -3,6 +3,8 @@ package com.gilsontsc.livariaapi.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -78,6 +80,39 @@ public class BookServiceTest {
         //verificar se o repository nunca deve chamou o metodo save
         Mockito.verify(repository, Mockito.never()).save(book);
 
+    }
+	
+	@Test
+    @DisplayName("Deve obter um livro por Id")
+    public void getByIdTest(){
+        Long id = 1l;
+        Book book = createValidBook();
+        book.setId(id);
+
+        when(repository.findById(id)).thenReturn(Optional.of(book));
+
+        //execucao
+        Optional<Book> foundBook = service.getById(id);
+
+        //verificacoes
+        assertThat(foundBook.isPresent() ).isTrue();
+        assertThat(foundBook.get().getId()).isEqualTo(id);
+        assertThat(foundBook.get().getAuthor()).isEqualTo(book.getAuthor());
+        assertThat(foundBook.get().getIsbn()).isEqualTo(book.getIsbn());
+        assertThat(foundBook.get().getTitle()).isEqualTo(book.getTitle());
+    }
+	
+	@Test
+    @DisplayName("Deve retornar vazio ao obter um livro por Id quando ele não existe na base.")
+    public void bookNotFoundByIdTest(){
+        Long id = 1l;
+        when(repository.findById(id)).thenReturn(Optional.empty());
+
+        //execucao
+        Optional<Book> book = service.getById(id);
+
+        //verificacoes
+        assertThat(book.isPresent()).isFalse();
     }
 	
 	private Book createValidBook() {
