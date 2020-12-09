01d3b44718fc30462dbb84df1746eacfc0da250f
gilsontsc <gilson.costa@dcx.ufpb.br>
2020-12-09 18:23:51 -0300

    Implementando o teste de update no controller

--- src/main/java/com/gilsontsc/livariaapi/api/controller/BookController.java
+++ src/main/java/com/gilsontsc/livariaapi/api/controller/BookController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -64,6 +65,16 @@ public class BookController {
 		BindingResult bindingResult = ex.getBindingResult();
 		return new ApiErrors(bindingResult);
 	}
+	
+	@PutMapping("{id}")
+    public BookDTO update(@PathVariable Long id, @RequestBody @Valid BookDTO dto){
+        return service.getById(id).map( book -> {
+            book.setAuthor(dto.getAuthor());
+            book.setTitle(dto.getTitle());
+            book = service.update(book);
+            return modelMapper.map(book, BookDTO.class);
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 	
 	@ExceptionHandler(BusinessException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
