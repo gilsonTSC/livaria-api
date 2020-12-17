9dd8d65f9dd77647b7664f2c5dbc0ab6535289a5
gilsontsc <gilson.costa@dcx.ufpb.br>
2020-12-17 18:48:09 -0300

    Teste de busca por empréstimo na camada de service

--- src/test/java/com/gilsontsc/livariaapi/service/LoanServiceTeste.java
+++ src/test/java/com/gilsontsc/livariaapi/service/LoanServiceTeste.java
@@ -7,16 +7,23 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.gilsontsc.livariaapi.api.dto.LoanFilterDTO;
 import com.gilsontsc.livariaapi.exception.BusinessException;
 import com.gilsontsc.livariaapi.model.entity.Book;
 import com.gilsontsc.livariaapi.model.entity.Loan;
@@ -129,6 +136,36 @@ public class LoanServiceTeste {
 
         assertThat(updatedLoan.getReturned()).isTrue();
         verify(repository).save(loan);
+    }
+	
+	@Test
+    @DisplayName("Deve filtrar empréstimos pelas propriedades")
+    public void findLoanTest(){
+        //cenario
+        LoanFilterDTO loanFilterDTO = LoanFilterDTO.builder().customer("Fulano").isbn("321").build();
+
+        Loan loan = createLoan();
+        loan.setId(1l);
+        PageRequest pageRequest = PageRequest.of(0, 10);
+        List<Loan> lista = Arrays.asList(loan);
+
+        Page<Loan> page = new PageImpl<Loan>(lista, pageRequest, lista.size());
+        when( repository.findByBookIsbnOrCustomer(
+                    Mockito.anyString(),
+                    Mockito.anyString(),
+                    Mockito.any(PageRequest.class))
+        )
+                .thenReturn(page);
+
+        //execucao
+        Page<Loan> result = service.find( loanFilterDTO, pageRequest );
+
+
+        //verificacoes
+        assertThat(result.getTotalElements()).isEqualTo(1);
+        assertThat(result.getContent()).isEqualTo(lista);
+        assertThat(result.getPageable().getPageNumber()).isEqualTo(0);
+        assertThat(result.getPageable().getPageSize()).isEqualTo(10);
     }
 	
 	public static Loan createLoan(){
